2341b59dd05c5dfc887878f4fceec2e7bf48bd4f
silver-xuu <silver.xu@outlook.com>
2020-02-25 15:11:55 -0800

    update homepage/guide mode UI

---
+++ Source/pocketPelvis/Assets/Scripts/buttonToggle.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buttonToggle : MonoBehaviour
+{
+    public void toggleItem(GameObject item)
+    {
+        item.SetActive(!item.activeSelf);
+    }
+}
