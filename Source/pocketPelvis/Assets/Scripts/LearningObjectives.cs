78bf5638211b4f90937e785f61dd8f2a93b3ea79
silver-xuu <silver.xu@outlook.com>
2020-03-06 12:40:02 -0800

    backend storage script for learning objective

---
+++ Source/pocketPelvis/Assets/Scripts/LearningObjectives.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LearningObjectives : MonoBehaviour
+{
+   
+
+    public LearningObject learningObject = new LearningObject();
+    private string jsonSavePath;
+    public static LearningObjectives instance;
+
+    private void Awake()
+    {
+
+        #if UNITY_EDITOR
+                jsonSavePath = Application.dataPath + "/SaveData/saveAchievements.json";
+        #elif UNITY_ANDROID || UNITY_IOS
+                jsonSavePath = Application.persistentDataPath + "/saveAchievements.json";
+        #endif
+        instance = this;
+        //DontDestroyOnLoad(this.gameObject);
+        LoadLOs();
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            ResetLOs();
+            SaveLOs();
+        }
+        
+    }
+    public void SaveLOs()
+    {
+        string save = JsonUtility.ToJson(learningObject);
+
+        System.IO.File.WriteAllText(jsonSavePath, save);
+    }
+    /// <summary>
+    /// Load learning objects from saved json file
+    /// </summary>
+    public void LoadLOs()
+    {
+        string load;
+        if (System.IO.File.Exists(jsonSavePath))
+        {
+            load = System.IO.File.ReadAllText(jsonSavePath);
+        }
+        else
+        {
+            load = System.IO.File.ReadAllText(Application.dataPath + "/SaveData/saveAchievements.json");
+        }
+        
+        learningObject= JsonUtility.FromJson<LearningObject>(load);
+    }
+    public void ResetLOs()
+    {
+        foreach(LOs lobject in learningObject.learningObjects)
+        {
+            for(int i = 0; i < lobject.learningObjectAchievement.Count; i++)
+            {
+                lobject.learningObjectAchievement[i] = false;
+            }
+        }
+    }
+}
+[System.Serializable]
+public class LearningObject
+{
+    public bool isNewUser;
+    public List<LOs> learningObjects = new List<LOs>();
+    
+}
+
+[System.Serializable]
+public class LOs
+{
+    public string objective;
+    public List<bool> learningObjectAchievement = new List<bool>();
+}
