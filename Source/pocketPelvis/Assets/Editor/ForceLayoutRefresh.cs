051fd16a7d92a1d87ee021fd2fde6ab810d07e44
Cara Meyers <caracmeyers@gmail.com>
2020-06-03 16:46:00 -0700

    added missing files and updated .gitignore

---
+++ Source/pocketPelvis/Assets/Editor/ForceLayoutRefresh.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ForceLayoutRefresh : MonoBehaviour
+{
+    public void refreshLayout ()
+    {
+        LayoutRebuilder.ForceRebuildLayoutImmediate(this.GetComponent<RectTransform>());
+    }
+}
