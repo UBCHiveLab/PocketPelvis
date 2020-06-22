8269fa02591bebaa1a55d377dd3fe80583463be9
Cara Meyers <caracmeyers@gmail.com>
2020-06-22 14:47:30 -0700

    Guide Mode: allow user to only select steps that they've been to 	- make step buttons grayed out and unclickable until the user has been to that step         - make menu stars and LO page stars unclickable and grayed out until user has been to step         - added ButtonInteractivityController to gray out and disable buttons         - track furthest step and furthest LO the user has been to, save to saveAchievments.json         - helped maintain LO data consistancy, by modifing LearningObjectives.cs so that the LearningObject cannot be modified directly by other classes. Other classes can only save, load, or reset LearningObject data 	- remove unused step buttons from main page

---
+++ Source/pocketPelvis/Assets/Scripts/GuideMode/ButtonInteractivityController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonInteractivityController : MonoBehaviour
+{
+    private readonly Color DISABLED_TINT = new Color(106.0f / 255.0f, 106.0f / 255.0f, 106.0f / 255.0f, 105 / 255.0f);
+    private readonly Color ENABLED_TINT = new Color(1, 1, 1, 1);
+
+    public void DisableButton(Button button, Image buttonImage = null)
+    {
+        button.interactable = false;
+        if (buttonImage != null)
+        {
+            buttonImage.color = DISABLED_TINT;
+        }
+    }
+
+    public void EnableButton(Button button, Image buttonImage = null)
+    {
+        button.interactable = true;
+        if (buttonImage != null)
+        {
+            buttonImage.color = ENABLED_TINT;
+        }
+    }
+}
--- Source/pocketPelvis/Assets/Scripts/GuideMode/LearningObjectives.cs
+++ Source/pocketPelvis/Assets/Scripts/GuideMode/LearningObjectives.cs
@@ -2,13 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public class LearningObject
+{
+    public bool isNewUser;
+    public int currentLO;
+    public int currentStep;
+    public int furthestLO;
+    public int furthestStep;
+    public List<LOs> learningObjects = new List<LOs>();
+
+}
+
+[System.Serializable]
+public class LOs
+{
+    public int id;
+    public string objective;
+    public List<bool> learningObjectAchievement = new List<bool>();
+}
+
 public class LearningObjectives : MonoBehaviour
 {
    
 
-    public LearningObject learningObject = new LearningObject();
+    private LearningObject learningObject = new LearningObject();
     private string jsonSavePath;
-    public static LearningObjectives instance; 
+    public static LearningObjectives instance;
+    public const int INTRO_STEP = 0;
 
     private void Awake()
     {
@@ -19,9 +40,9 @@ public class LearningObjectives : MonoBehaviour
                 jsonSavePath = Application.persistentDataPath + "/saveAchievements.json";
         #endif
         instance = this;
-        //DontDestroyOnLoad(this.gameObject);
         LoadLOs();
     }
+
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.A))
@@ -29,19 +50,18 @@ public class LearningObjectives : MonoBehaviour
             ResetLOs();
             SaveLOs();
         }
-        //Debug.Log("Current lo step:" + learningObject.lastLO + "-" + learningObject.lastStep);
     }
-    public void SaveLOs()
+
+    private void SaveLOs()
     {
         string save = JsonUtility.ToJson(learningObject);
 
         System.IO.File.WriteAllText(jsonSavePath, save);
     }
-    /// <summary>
-    /// Load learning objects from saved json file
-    /// </summary>
-    public void LoadLOs()
+
+    private void LoadLOs()
     {
+        // Load learning objects from saved json file
         string load;
         if (System.IO.File.Exists(jsonSavePath))
         {
@@ -54,6 +74,7 @@ public class LearningObjectives : MonoBehaviour
         
         learningObject= JsonUtility.FromJson<LearningObject>(load);
     }
+
     public void ResetLOs()
     {
         foreach(LOs lobject in learningObject.learningObjects)
@@ -64,25 +85,109 @@ public class LearningObjectives : MonoBehaviour
             }
         }
         learningObject.isNewUser = true;
-        learningObject.lastLO = 0;
-        learningObject.lastStep = 0;
+        learningObject.currentLO = 0;
+        learningObject.currentStep = 0;
+        learningObject.furthestLO = 0;
+        learningObject.furthestStep = 0;
         SaveLOs();
     }
-}
-[System.Serializable]
-public class LearningObject
-{
-    public bool isNewUser;
-    public int lastLO;
-    public int lastStep;
-    public List<LOs> learningObjects = new List<LOs>();
-    
-}
 
-[System.Serializable]
-public class LOs
-{
-    public int id;
-    public string objective;
-    public List<bool> learningObjectAchievement = new List<bool>();
+    public void UpdateLOProgress(int currentLO, int currentStep)
+    {
+        // when the user makes progress, they shouldn't be considered a new user
+        learningObject.isNewUser = false;
+
+        learningObject.currentLO = currentLO;
+        learningObject.currentStep = currentStep;
+
+        if (currentLO > learningObject.furthestLO)
+        {
+            // if currently on a further lo, make this lo and the step the furthest we have been to.
+            // if the current step is the intro step, make the furthest step the first step of the lo
+            learningObject.furthestLO = currentLO;
+            learningObject.furthestStep = currentStep == INTRO_STEP ? INTRO_STEP + 1 : currentStep;
+
+        } else if (currentLO == learningObject.furthestLO &&  currentStep > learningObject.furthestStep)
+        {
+            // if on the same lo as the furthest one and we have gone to a further step, make this step the furthest.
+            learningObject.furthestStep = currentStep;
+        }
+
+        SaveLOs();
+    }
+
+    public void AchieveLOStep(int loIndex, int stepIndex)
+    {
+        if (!IsValidStepIndex(loIndex, stepIndex))
+        {
+            return;
+        }
+        learningObject.learningObjects[loIndex].learningObjectAchievement[stepIndex] = true;
+        SaveLOs();
+    }
+
+    public bool HaveBeenToStep(int lo, int step)
+    {
+        // either, we've been to all the steps of this lo before or we've been to this step before
+        return lo < learningObject.furthestLO || (lo == learningObject.furthestLO && step <= learningObject.furthestStep);
+    }
+
+    public int GetLearningObjectiveId(int loIndex)
+    {
+        return IsValidLOIndex(loIndex) ? learningObject.learningObjects[loIndex].id : 0;
+    }
+
+    public int GetNumberOfLearningObjectives()
+    {
+        return learningObject.learningObjects.Count;
+    }
+
+    public int GetCurrentLearningObjective()
+    {
+        return learningObject.currentLO;
+    }
+
+    public int GetCurrentStep()
+    {
+        return learningObject.currentStep;
+    }
+
+    public int GetFurthestLearningObjective()
+    {
+        return learningObject.furthestLO;
+    }
+
+    public int GetFurthestStep()
+    {
+        return learningObject.furthestStep;
+    }
+
+    public int GetNumberOfSteps(int loIndex)
+    {
+        return IsValidLOIndex(loIndex) ? learningObject.learningObjects[loIndex].learningObjectAchievement.Count : 0;
+    }
+
+    public bool IsNewUser()
+    {
+        return learningObject.isNewUser;
+    }
+
+    public bool HaveAchievedStep(int loIndex, int stepIndex)
+    {
+        return IsValidStepIndex(loIndex, stepIndex) ? learningObject.learningObjects[loIndex].learningObjectAchievement[stepIndex] : false;
+    }
+
+    private bool IsValidLOIndex(int loIndex)
+    {
+        return loIndex > -1 && learningObject.learningObjects.Count > loIndex;
+    }
+
+    private bool IsValidStepIndex(int loIndex, int stepIndex)
+    {
+        if (IsValidLOIndex(loIndex))
+        {
+            return stepIndex > -1 && learningObject.learningObjects[loIndex].learningObjectAchievement.Count > stepIndex;
+        }
+        return false;
+    }
 }
