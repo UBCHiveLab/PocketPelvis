99322cb1fe97e157f32f46b7198afe3b26d441b1
Cara Meyers <caracmeyers@gmail.com>
2020-07-20 17:25:59 -0700

    Guide Mode Refactor:             - Added StarButtonsSpriteUpdater to update star button sprites to reflect user progress             - Changed execution order of SaveDataManager and GuideModeEventManager, so that they get awoken before all other scripts in scene             - TODO: fix bugs where star sprites don't get updated when menu is open or when user reset

---
+++ Source/pocketPelvis/Assets/Scripts/GuideMode/StarButtonsSpriteUpdater.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class StarButtonsSpriteUpdater : MonoBehaviour
+{
+    public Sprite achievedSprite, inProgressAndAchievedSprite, inProgressSprite, notDoneSprite;
+    private SaveDataManager saveDataManager;
+
+    private void Awake()
+    {
+        saveDataManager = SaveDataManager.Instance;
+    }
+
+    private void OnEnable()
+    {
+        UpdateSprites();
+    }
+
+    private void UpdateSprites()
+    {
+        // get the latest user data to ensure that the sprites are updated correctly
+        UserSaveData userData = saveDataManager.GetCurrentSaveState();
+        UserProgressData userProgress = userData.userProgress;
+
+        int loIndex = 0;
+        userData.loAchievements.ForEach((loData) =>
+        {
+            Transform learningObjectiveTransform = transform.GetChild(loIndex);
+            int lo = loData.learningObjective;
+            int stepIndex = 0;
+
+            loData.stepAchievementStatus.ForEach((stepAchieved) =>
+            {
+                Transform loStepTransform = learningObjectiveTransform.GetChild(stepIndex);
+                Image loStepImg = loStepTransform.GetComponent<Image>();
+                Button loStepButton = loStepTransform.GetComponent<Button>();
+                int step = stepIndex + 1;
+
+                // update the step's sprite to match the user's latest progress through the learning objectives
+                loStepImg.sprite = StepIsInProgress(userProgress, lo, step) ?
+                    stepAchieved ? inProgressAndAchievedSprite : inProgressAndAchievedSprite :
+                    stepAchieved ? achievedSprite : notDoneSprite;
+
+                // allow the button for the learning objective be interacted with only if we've been to the step before
+                if (lo < userProgress.furthestLO || (lo == userProgress.furthestLO && step <= userProgress.furthestStep))
+                {
+                    ButtonInteractivityController.EnableButton(loStepButton);
+                }
+                else
+                {
+                    ButtonInteractivityController.DisableButton(loStepButton);
+                }
+
+                stepIndex++;
+            });
+
+            loIndex++;
+        });
+    }
+
+    private bool StepIsInProgress(UserProgressData userProgress, int learningObjective, int step)
+    {
+        // There's no sprite for the lo introduction. When the user is on the introduction, mark their progress as the lo's first step
+        bool stepIsIntro = userProgress.currentStep == SaveDataManager.INTRO_STEP && step == SaveDataManager.INTRO_STEP + 1;
+
+        bool stepIsSelected = step == userProgress.currentStep;
+        return (stepIsIntro || stepIsSelected) && learningObjective == userProgress.currentLO;
+    }
+}
