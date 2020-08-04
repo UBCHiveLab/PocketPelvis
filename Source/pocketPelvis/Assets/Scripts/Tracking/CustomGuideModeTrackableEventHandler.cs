7dc373aa96690be408e7eff430ada9ae7884ed4a
Cara Meyers <caracmeyers@gmail.com>
2020-08-04 12:58:26 -0700

    Guide Mode Refactor: Prevent Publication of Tracking Status Changed Event When Guide Mode Event Manager is Destroyed 	- check if GuideModeEventManager still exists before publishing event 	- renamed parameter to make it clearer what is happening with OnModelTrackingStatusChanged and PublishModelTrackingChangedEvent

--- Source/pocketPelvis/Assets/Scripts/Tracking/CustomGuideModeTrackableEventHandler.cs
+++ Source/pocketPelvis/Assets/Scripts/Tracking/CustomGuideModeTrackableEventHandler.cs
@@ -1,27 +1,34 @@
-﻿
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using Vuforia;
-public class CustomGuideModeTrackableEventHandler : DefaultTrackableEventHandler
+﻿public class CustomGuideModeTrackableEventHandler : DefaultTrackableEventHandler
 {
+    GuideModeEventManager eventManager;
+
+    private void Awake()
+    {
+        eventManager = GuideModeEventManager.Instance;
+    }
+
     protected override void OnTrackingFound()
     {
         base.OnTrackingFound();
-        //set progress to win
         ChangeTrackingStatus(true);
     }
+
     protected override void OnTrackingLost()
     {
         base.OnTrackingLost();
         ChangeTrackingStatus(false);
     }
-    #region CUSTOM_METHODS
-    private void ChangeTrackingStatus(bool trackingStatus)
-    {
-        GuideModeEventManager.Instance.PublishModelTrackingChangedEvent(trackingStatus);
-    }
 
+    #region CUSTOM_METHODS
+    private void ChangeTrackingStatus(bool isTrackingModel)
+    {
+        if (eventManager != null)
+        {
+            // when the program quits while the model is being tracked, then the tracking status will change, but the event manager may already be destroyed
+            // only alert all subscribers that the tracking status has changed if the event manager and all of it's events have not been destroyed
+            eventManager.PublishModelTrackingChangedEvent(isTrackingModel);
+        }
+    }
     #endregion
 }
 
