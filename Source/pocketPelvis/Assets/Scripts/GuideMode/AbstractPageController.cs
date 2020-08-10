534b33314aba899a9c107bc5062ce4297b49e990
Cara Meyers <caracmeyers@gmail.com>
2020-08-10 14:55:19 -0700

    Guide Mode Refactor: Use PageManager to Update Active Page UI

---
+++ Source/pocketPelvis/Assets/Scripts/GuideMode/AbstractPageController.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public abstract class AbstractPageController : MonoBehaviour
+{
+    public PageType pageType;
+
+    /// <summary> Update the UI to reflect the user's current progress </summary>
+    public abstract void UpdateUI(UserProgressData currentProgress, LOTexts loTexts);
+
+    /// <summary> Update the UI to show whether the pelvis model is currently being tracked or not </summary>
+    public abstract void UpdateUI(bool isTrackingModel);
+}
---
+++ Source/pocketPelvis/Assets/Scripts/GuideMode/LoPageController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class LoPageController : AbstractPageController
+{
+    [SerializeField]
+    private Text startButtonTxt;
+
+    private PanelManager panelManager;
+
+    private const string START_TXT = "START LEARNING";
+    private const string RESUME_TXT = "RESUME LEARNING";
+
+    private void Awake()
+    {
+        panelManager = PanelManager.Instance;
+    }
+
+    public override void UpdateUI(UserProgressData currentProgress, LOTexts loTexts)
+    {
+        panelManager.SetDefaultPanelType(null); // when no panels are visible, make no panel appear as the default panel
+        startButtonTxt.text = currentProgress.isNewUser ? START_TXT : RESUME_TXT;
+    }
+
+    public override void UpdateUI(bool isTrackingModel)
+    {
+        // for now, the lo page does not update when the tracking status of the model changes
+        return;
+    }
+}
