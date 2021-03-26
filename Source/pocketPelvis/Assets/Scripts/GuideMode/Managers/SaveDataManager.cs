943d6fcac36f705762398be1d905eea54af192aa
Steve_Zhang <wentaozhang0305@gmail.com>
2021-03-26 16:52:13 -0700

    added materials for models; noted: data bug for ios still exists

--- Source/pocketPelvis/Assets/Scripts/GuideMode/Managers/SaveDataManager.cs
+++ Source/pocketPelvis/Assets/Scripts/GuideMode/Managers/SaveDataManager.cs
@@ -21,9 +21,11 @@ public class SaveDataManager : SceneSingleton<SaveDataManager>
 
         #if UNITY_EDITOR
             saveDataPath = Application.dataPath + "/SaveData/" + SAVE_DATA_PATH;
-#elif UNITY_ANDROID || UNITY_IOS
+#elif UNITY_ANDROID 
             saveDataPath = Application.persistentDataPath + "SAVE_DATA_PATH";
             //saveDataPath = Path.Combine(Application.persistantDataPath，“SAVE_DATA_PATH”);
+#elif UNITY_IOS
+            saveDataPath = Application.persistentDataPath + "/SaveData" + SAVE_DATA_PATH;
 #endif
 
         LoadSaveData();
